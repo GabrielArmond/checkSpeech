a0ba195c9892b959ef0bb50eaa781b3281e9af98
Gabriel Armond <gabriel.guerra@farme.com.br>
2022-07-19 01:47:35 -0300

    fix: cookies

--- src/components/cookies/index.js
+++ src/components/cookies/index.js
@@ -6,36 +6,29 @@ const Cookies = () => {
   const { handleAcceptCookie } = useCookie()
 
   return (
-    <Fragment>
-      <div className="row cookie-bar px-5 py-2 align-items-center">
-        <div className="col-12 col-md-8 col-sm-12 pl-5">
+    <>
+      <div className="row cookie-bar px-5 py-2 fixed-bottom align-items-center">
+        <div className="col-12 col-md-6 col-sm-12">
           <span className="text-start">
-            Usamos cookies necessários para o funcionamento do site.
+            Este website usa cookies necessários para o funcionamento do mesmo.
           </span>
-          <p>
-            Ao continuar a usar este site, você concorda com nossos cookies.
-          </p>
-          <p>Leia nossa política para saber mais.</p>
-          <a href="/" className="text-light">
-            Saber mais
-          </a>
         </div>
-        <div className="col-12 col-md-4 col-sm-12">
+        <div className="col-12 col-md-6 col-sm-12">
           <button
-            className="btn btn-outline-light p-2 m-2"
+            className="btn btn-outline-light m-2"
             onClick={() => handleAcceptCookie(false)}
           >
             Rejeitar cookies
           </button>
           <button
-            className="btn btn-primary p-2"
+            className="btn btn-primary m-2"
             onClick={() => handleAcceptCookie(true)}
           >
             Aceitar cookies
           </button>
         </div>
       </div>
-    </Fragment>
+    </>
   )
 }
 
